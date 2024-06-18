418af273c20965dcc2b5ba3bb05db0e70b8176bb
Nguyen Anh Quan <anhq46724@gmail.com>
2024-06-18 15:52:56 +0700

    fix kho

--- QlBanHang/MiniMart/MiniMart/PresentationLayer/Forms/FormKho.Designer.cs
+++ QlBanHang/MiniMart/MiniMart/PresentationLayer/Forms/FormKho.Designer.cs
@@ -168,7 +168,7 @@
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // TimKiemComboBox
+            // TimKiemComboBox 
             // 
             this.TimKiemComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.TimKiemComboBox.FormattingEnabled = true;
